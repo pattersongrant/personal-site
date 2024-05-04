83e8d80ea930046c4addeb01fc4094ba900454d3
Grant Patterson <pattersongrant06@gmail.com>
2024-05-03 22:00:19 -0400

    linkedin and email

--- my-site/app/page.tsx
+++ my-site/app/page.tsx
@@ -5,10 +5,20 @@ export default function Home() {
     <main className="h-full p-10 flex items-end">
         <div className="my-auto">
         <h1 className="sm:text-5xl md:text-9xl text-3xl text-white font-sans font-bold align-middle mx-auto my-6 hover:text-sky-800 duration-200">Grant Patterson</h1>
-        <a className="flex hover:invert duration-200"href="https://github.com/pattersongrant">
-        <img src="git.png" className="w-8 my-auto"></img>
-        <h4 className="mx-2 text-black my-auto">pattersongrant</h4>
-        </a>
+          <div className="flex">
+            <a className="inline-flex invert hover:invert-0 duration-200" href="https://github.com/pattersongrant">
+            <img src="git.png" className="w-8 my-auto"></img>
+            <h4 className="mx-2 inline text-black my-auto">pattersongrant</h4>
+            </a>
+            <a className="inline-flex invert hover:invert-0 duration-200" href="https://www.linkedin.com/in/pattersongrant/">
+            <img src="linkedin.png" className="w-8 my-auto"></img>
+            <h4 className="mx-2 inline text-black my-auto">pattersongrant</h4>
+            </a>
+            <a className="inline-flex invert hover:invert-0 duration-200" href="mailto:pattersongrant06@gmail.com">
+            <img src="email.png" className="w-8 my-auto"></img>
+            <h4 className="mx-2 inline text-black my-auto">pattersongrant06@gmail.com</h4>
+            </a>
+          </div>
         </div>
         <div className="w-2/5 ml-auto">
           <img className="m-2 hover:drop-shadow-lg hover:text-gold duration-100 mx-auto w-3/12" src="/tottenham.png" alt="tottenham logo" />
