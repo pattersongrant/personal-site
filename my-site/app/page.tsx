6365146d6f2dd25760e98f2e86c2a83d9f127a52
Grant Patterson <pattersongrant06@gmail.com>
2024-09-04 23:04:58 -0400

    new table

--- my-site/app/page.tsx
+++ my-site/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <div className="sm:w-2/5 ml-auto my-auto">
           <img className="m-2 hover:drop-shadow-lg hover:text-gold duration-100 mx-auto w-0 sm:min-w-28 sm:w-3/12" src="/tottenham.png" alt="tottenham logo" />
           <div className="shadow-2xl" id="scoreaxis-widget-96e18" style={{ borderWidth: '0px', borderColor: 'rgba(0, 0, 0, 0.15)', borderStyle: 'solid', borderRadius: '8px', padding: '10px', background: 'rgb(255, 255, 255)', width: '100%', container: '1px', maxHeight: '80vh', overflow: 'auto'}}>
-            <iframe id="Iframe" src="https://www.scoreaxis.com/widget/standings-widget/8?autoHeight=1&amp;widgetHomeAwayTabs=0&amp;header=0&amp;widgetRows=1%2C1%2C1%2C1%2C1%2C1%2C0%2C1%2C1%2C1&amp;borderColor=%23ffffff&amp;lang=en&amp;font=0&amp;teamsLogo=1&amp;removeBorders=0&amp;fontSize=&amp;links=1&amp;inst=96e18" style={{ width: '100%', border: 'none', transition: 'all 300ms ease' }}></iframe>
+            <iframe id="Iframe" src="https://www.scoreaxis.com/widget/standings-widget/8?autoHeight=1&amp;widgetRows=1%2C1%2C1%2C1%2C1%2C1%2C0%2C1%2C1%2C1&amp;removeBorders=0&amp;header=1&amp;widgetHomeAwayTabs=0&amp;borderColor=%23ffffff&amp;inst=3f7b5" style={{ width: '100%', border: 'none', transition: 'all 300ms ease' }}></iframe>
             <script dangerouslySetInnerHTML={{ __html: `window.addEventListener("DOMContentLoaded",event=>{window.addEventListener("message",event=>{if(event.data.appHeight&&"96e18"==event.data.inst){let container=document.querySelector("#scoreaxis-widget-96e18 iframe");container&&(container.style.height=parseInt(event.data.appHeight)+"px")}},!1)});` }}></script>
           </div>
         </div>
