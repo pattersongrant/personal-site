348e75fa6b4eb2c8bf578892c90daf7eb7a3bff1
Grant Patterson <pattersongrant06@gmail.com>
2024-09-04 23:57:54 -0400

    test2

--- my-site/app/page.tsx
+++ my-site/app/page.tsx
@@ -22,10 +22,7 @@ export default function Home() {
         </div>
         <div className="sm:w-2/5 ml-auto my-auto">
           <img className="m-2 hover:drop-shadow-lg hover:text-gold duration-100 mx-auto w-0 sm:min-w-28 sm:w-3/12" src="/tottenham.png" alt="tottenham logo" />
-          <div className="shadow-2xl" id="scoreaxis-widget-96e18" style={{ borderWidth: '0px', borderColor: 'rgba(0, 0, 0, 0.15)', borderStyle: 'solid', borderRadius: '8px', padding: '10px', background: 'rgb(255, 255, 255)', width: '100%', container: '1px', maxHeight: '80vh', overflow: 'auto'}}>
-            <iframe id="Iframe" src="https://www.scoreaxis.com/widget/standings-widget/8?autoHeight=1&amp;widgetRows=1%2C1%2C1%2C1%2C1%2C1%2C0%2C1%2C1%2C1&amp;removeBorders=0&amp;header=1&amp;widgetHomeAwayTabs=0&amp;borderColor=%23ffffff&amp;inst=3f7b5" style={{ width: '100%', border: 'none', transition: 'all 300ms ease' }}></iframe>
-            <script dangerouslySetInnerHTML={{ __html: `window.addEventListener("DOMContentLoaded",event=>{window.addEventListener("message",event=>{if(event.data.appHeight&&"96e18"==event.data.inst){let container=document.querySelector("#scoreaxis-widget-96e18 iframe");container&&(container.style.height=parseInt(event.data.appHeight)+"px")}},!1)});` }}></script>
-          </div>
+          <iframe className="w-full h-full rounded-lg" height="500px" src = "https://www.scoreaxis.com/widget/standings-widget/8?autoHeight=1&amp;widgetRows=1%2C1%2C1%2C1%2C1%2C1%2C0%2C1%2C1%2C1&amp;removeBorders=0&amp;header=1&amp;widgetHomeAwayTabs=0&amp;borderColor=%23ffffff&amp;inst=3f7b5"></iframe>
         </div>
     </main>
   );
