d3fd7654832ba80bafd8052d1dd972d486ee12ae
Grant Patterson <pattersongrant06@gmail.com>
2024-09-04 23:46:03 -0400

    reset

--- my-site/app/page.tsx
+++ my-site/app/page.tsx
@@ -26,9 +26,6 @@ export default function Home() {
             <iframe id="Iframe" src="https://www.scoreaxis.com/widget/standings-widget/8?autoHeight=1&amp;widgetRows=1%2C1%2C1%2C1%2C1%2C1%2C0%2C1%2C1%2C1&amp;removeBorders=0&amp;header=1&amp;widgetHomeAwayTabs=0&amp;borderColor=%23ffffff&amp;inst=3f7b5" style={{ width: '100%', border: 'none', transition: 'all 300ms ease' }}></iframe>
             <script dangerouslySetInnerHTML={{ __html: `window.addEventListener("DOMContentLoaded",event=>{window.addEventListener("message",event=>{if(event.data.appHeight&&"96e18"==event.data.inst){let container=document.querySelector("#scoreaxis-widget-96e18 iframe");container&&(container.style.height=parseInt(event.data.appHeight)+"px")}},!1)});` }}></script>
           </div>
-          <div id="scoreaxis-widget-f3d6a">
-            <iframe id="Iframe" src="https://www.scoreaxis.com/widget/standings-widget/8?autoHeight=1&amp;widgetHomeAwayTabs=0&amp;borderColor=%23ffffff&amp;inst=f3d6a"></iframe>
-            <script>window.addEventListener "DOMContentLoaded";</script></div>
         </div>
     </main>
   );
