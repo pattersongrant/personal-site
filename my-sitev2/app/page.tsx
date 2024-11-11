d40060bd0502850a7206c73501f26f9c84fb7920
Grant Patterson <pattersongrant06@gmail.com>
2024-11-11 15:41:05 -0500

    more professional

--- my-sitev2/app/page.tsx
+++ my-sitev2/app/page.tsx
@@ -3,31 +3,26 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-3xl font-semibold tracking-tighter">
+      <h1 className="mb-4 text-4xl font-semibold tracking-tighter">
         Grant Patterson
       </h1>
       <p className="mb-4">
-        {`Software Engineer`}
+        <h1 className="text-2xl">Software Engineer &</h1>
+        <h1 className="text-2xl">CS Student at the University of Michigan in Ann Arbor</h1>
         <br></br>
-        {'Computer Science Student at the University of Michigan in Ann Arbor'}
+        {'Nice to meet you!'}
         <br></br>
+        {'Love building, friends, family, reading, gym, sports, studying, competition.'}
         <br></br>
-        {'Nice to meet you'}
+        {'Hope to do and make some great big things in the world.'}
         <br></br>
-        {'Love building, friends, family, reading, gym, sports, studying, competition'}
-        <br></br>
-        {'Hope to do and make some great big things in the world'}
-        <br></br><br></br>
-        {'Fun fact:'}
-        <br></br>
-        {'I was born in santa monica, CA.'}
         
         
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
-      <img className="w-3/5"src="/fish.jpeg"></img>
+      <img className="w-2/5"src="/fish.jpeg"></img>
     </section>
 
   )
