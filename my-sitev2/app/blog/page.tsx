b5a213101c8f6d869908e8bd5eb6c1e52e067d67
Grant Patterson <pattersongrant06@gmail.com>
2024-12-21 05:50:37 -0500

    fixed blog title

--- my-sitev2/app/blog/page.tsx
+++ my-sitev2/app/blog/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Grant's Blog</h1>
+      <h1 className="font-semibold text-3xl mb-8 tracking-tighter">Grant's Blog</h1>
       <BlogPosts />
     </section>
   )
